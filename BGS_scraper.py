3aecf64bb5e8e412ed6855f63c89a497722b3076
gohu00 <51415312+gohu00@users.noreply.github.com>
2023-01-10 22:19:49 +0100

    Update BGS_scraper.py

--- BGS_scraper.py
+++ BGS_scraper.py
@@ -1,3 +1,5 @@
+import urllib
+
 def BGS_scraper (datee, datea, commodity) :
   url = f"https://www2.bgs.ac.uk/mineralsuk/statistics/wms.cfc?method=listResults&dataType=Production&dateFrom={datea}&dateTo={datee}&commodity={commodity}&country=&agreeToTsAndCs=agreed&exportToSpreadsheet=Yes"
   urllib.request.urlretrieve(url, "test.xlsx")
